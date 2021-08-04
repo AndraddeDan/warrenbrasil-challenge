9623006207d77eb77a86718e03c646ae0a163b6f
Daniel Andrade <andraddedan@icloud.com>
2021-08-04 09:58:48 -0300

    refactor: change button component to mixin

--- src/components/Btn/index.ts
+++
@@ -1,7 +0,0 @@
-import type { Component } from "vue";
-
-export const Btn = (): Promise<Component> => {
-  return import(
-    /* webpackChunkName: "Btn" */ "./Btn.vue"
-  ) as Promise<Component>;
-};
