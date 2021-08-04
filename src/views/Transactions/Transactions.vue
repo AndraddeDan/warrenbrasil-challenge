f73d169565aee4f13eedf1d28355eac1ae8d34ec
Daniel Andrade <andraddedan@icloud.com>
2021-08-04 01:04:07 -0300

    feat: adds searcher component into Transactions page

--- src/views/Transactions/Transactions.vue
+++ src/views/Transactions/Transactions.vue
@@ -2,6 +2,9 @@
   <div class="Transactions">
     <Header title="Transações">
       <FilterByStatus />
+      <template v-slot:action>
+        <Searcher placeholder="Busque pelo título" />
+      </template>
     </Header>
   </div>
 </template>
@@ -10,12 +13,14 @@
 import { Component, Vue } from "vue-property-decorator";
 import { Header } from "@/components/Header";
 import { FilterByStatus } from "@/components/FilterByStatus";
+import { Searcher } from "@/components/Searcher";
 
 @Component({
   name: "Transactions",
   components: {
     Header,
     FilterByStatus,
+    Searcher,
   },
 })
 export default class Transactions extends Vue {
