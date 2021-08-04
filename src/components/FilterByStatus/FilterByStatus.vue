7c2a64e74c7644be4496904b8fb3c0c47a28db45
Daniel Andrade <andraddedan@icloud.com>
2021-08-04 02:11:20 -0300

    feat: adds buttons to filter

--- src/components/FilterByStatus/FilterByStatus.vue
+++ src/components/FilterByStatus/FilterByStatus.vue
@@ -1,28 +1,45 @@
 <template>
-  <section class="FilterByStatus">oi</section>
+  <section class="FilterByStatus">
+    <Btn> Todos </Btn>
+    <Btn
+      v-for="(index, status) in Object.keys(TransactionStatus)"
+      :key="status"
+    >
+      {{ TransactionStatus[index] }}
+    </Btn>
+  </section>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { Btn } from "@/components/Btn";
+import { TransactionStatus } from "@/models";
 
 @Component({
   name: "FilterByStatus",
+  components: {
+    Btn,
+  },
 })
-export default class FilterByStatus extends Vue {}
+export default class FilterByStatus extends Vue {
+  public TransactionStatus = TransactionStatus;
+}
 </script>
 
 <style scoped lang="less">
 .FilterByStatus {
-  background: black;
-  color: @secondary-text-color;
-  height: 50px;
-  padding: 10px 30px;
+  height: 60px;
   display: flex;
-  align-items: flex-end;
+  align-items: flex-start;
+  justify-content: center;
+  flex-wrap: wrap;
+  .sm({
+    height: 90px;
+    margin: 15px;
+  });
 
-  &__title {
-    font-weight: 100;
-    margin: 0;
+  button {
+    margin: 5px;
   }
 }
 </style>
