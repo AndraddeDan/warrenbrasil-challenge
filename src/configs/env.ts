981592aa6ef8e612266d18b87b7b29313b6908c2
Daniel Andrade <andraddedan@icloud.com>
2021-08-06 00:16:21 -0300

    feat: adds .env file

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -1,4 +1,5 @@
 export default {
   apiURL: process.env.VUE_APP_API_URL || "http://localhost:3333",
   production: process.env.NODE_ENV === "production",
+  development: process.env.NODE_ENV === "development",
 };
