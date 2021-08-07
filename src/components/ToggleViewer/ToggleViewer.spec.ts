1e5586e6d2e6373798f8602d4ac9ed665fb0f9ec
Daniel Andrade <andraddedan@icloud.com>
2021-08-07 13:32:22 -0300

    test: adds the ToggleViewer tests

--- src/components/ToggleViewer/ToggleViewer.spec.ts
+++ src/components/ToggleViewer/ToggleViewer.spec.ts
@@ -1,12 +1,82 @@
 import { shallowMount } from "@vue/test-utils";
-import ToggleViewer from "@/components/ToggleViewer/ToggleViewer.vue";
+import ToggleViewer from "./ToggleViewer.vue";
+
+import ClosedEyeIcon from "@/assets/icons/ClosedEyeIcon.svg";
+import OpenedEyeIcon from "@/assets/icons/OpenedEyeIcon.svg";
 
 describe("ToggleViewer.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
+  it("checks if only OpenedEye icon is render when  props.canShow is true", () => {
     const wrapper = shallowMount(ToggleViewer, {
-      propsData: { msg },
+      propsData: { canShow: true },
     });
-    expect(wrapper.text()).toMatch(msg);
+
+    const hasOpenedEye = wrapper.findComponent(OpenedEyeIcon);
+    const hasClosedEye = wrapper.findComponent(ClosedEyeIcon);
+
+    expect(hasOpenedEye.exists()).toBe(true);
+    expect(hasClosedEye.exists()).toBe(false);
+  });
+
+  it("checks if only ClosedEye icon is render when  props.canShow is false", () => {
+    const wrapper = shallowMount(ToggleViewer, {
+      propsData: { canShow: false },
+    });
+
+    const hasOpenedEye = wrapper.findComponent(OpenedEyeIcon);
+    const hasClosedEye = wrapper.findComponent(ClosedEyeIcon);
+
+    expect(hasOpenedEye.exists()).toBe(false);
+    expect(hasClosedEye.exists()).toBe(true);
+  });
+
+  it("tests if the click trigger emit an event", async () => {
+    const wrapper = shallowMount(ToggleViewer, {
+      propsData: { canShow: false },
+    });
+
+    const hasOpenedEye = wrapper.findComponent(OpenedEyeIcon);
+    const hasClosedEye = wrapper.findComponent(ClosedEyeIcon);
+
+    expect(hasOpenedEye.exists()).toBe(false);
+    expect(hasClosedEye.exists()).toBe(true);
+  });
+
+  it("tests if the click trigger emit an event", async () => {
+    const wrapper = shallowMount(ToggleViewer, {
+      propsData: { canShow: false },
+    });
+
+    await wrapper.trigger("click");
+
+    const change = wrapper.emitted().change;
+
+    expect(change).toBeTruthy();
+  });
+
+  it("tests if the click trigger call the method", async () => {
+    const wrapper = shallowMount(ToggleViewer, {
+      propsData: { canShow: false },
+    });
+
+    const output = jest.fn();
+    wrapper.setMethods({ output });
+
+    await wrapper.trigger("click");
+
+    expect(output).toBeCalled();
+  });
+
+  it("tests if method param inverse to the canShow prop when the method is called", async () => {
+    const canShow = true;
+    const wrapper = shallowMount(ToggleViewer, {
+      propsData: { canShow },
+    });
+
+    const output = jest.fn();
+    wrapper.setMethods({ output });
+
+    await wrapper.trigger("click");
+
+    expect(output.mock.calls[0][0]).toBe(!canShow);
   });
 });
