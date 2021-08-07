143da9b9ae4ccbb53a776bffc06cdd65afb26368
Daniel Andrade <andraddedan@icloud.com>
2021-08-07 12:53:40 -0300

    chore(configs): adds a jest config to use svg in tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
   testMatch: ["**/*.spec.{j,t}s"],
+  transform: {
+    "^.+\\.svg$": "<rootDir>/svg-transform.js",
+  },
 };
