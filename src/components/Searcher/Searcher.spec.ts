2fc9248c8e8f55c11f59ba92a72d1942d9c08abf
Daniel Andrade <andraddedan@icloud.com>
2021-08-07 15:39:51 -0300

    test: adds the Searcher props tests

--- src/components/Searcher/Searcher.spec.ts
+++ src/components/Searcher/Searcher.spec.ts
@@ -1,12 +1,30 @@
 import { shallowMount } from "@vue/test-utils";
-import Searcher from "@/components/Header/Searcher.vue";
+import Searcher from "./Searcher.vue";
 
 describe("Searcher.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
+  const searchSelector = '[data-testid="Searcher-input"]';
+
+  it("renders props.placeholder when passed", () => {
+    const placeholder = "search here";
     const wrapper = shallowMount(Searcher, {
-      propsData: { msg },
+      propsData: { placeholder },
     });
-    expect(wrapper.text()).toMatch(msg);
+
+    const input = wrapper.find(searchSelector);
+
+    expect(input.element.getAttribute("placeholder")).toMatch(placeholder);
+    expect(wrapper.vm.$props.placeholder).toMatch(placeholder);
+  });
+
+  it("renders props.value when passed", () => {
+    const value = "searched value";
+    const wrapper = shallowMount(Searcher, {
+      propsData: { value },
+    });
+
+    const input = <HTMLInputElement>wrapper.find(searchSelector).element;
+
+    expect(input.value).toMatch(value);
+    expect(wrapper.vm.$props.value).toMatch(value);
   });
 });
