7e067fdf716f0a5f923f34cac2a5bf9d573d5864
Daniel Andrade <andraddedan@icloud.com>
2021-08-08 02:34:26 -0300

    fix(test): TimelineStatus test not was been called, but now is fixed

--- src/components/TimelineStatus/TimelineStatus.spec.ts
+++ src/components/TimelineStatus/TimelineStatus.spec.ts
@@ -13,16 +13,19 @@ describe("TimelineStatus.vue", () => {
     expect(wrapper.vm.$props.status).toMatch(status);
   });
 
-  it("renders css class after 500ms", () => {
+  it("renders css class after 500ms", (done) => {
     const status = TransactionStatus.created;
     const wrapper = shallowMount(TimelineStatus, {
       propsData: { status },
     });
 
     const bar = wrapper.find('[data-testid="TimelineStatus-bar"]');
-    const cssClass = `imelineStatus-Line__bar--${normalizeUtils(status)}`;
+    const cssClass = `TimelineStatus-Line__bar--${normalizeUtils(status)}`;
 
     expect(bar.classes(cssClass)).toBe(false);
-    setTimeout(() => expect(bar.classes(cssClass)).toBe(true), 500);
+    setTimeout(() => {
+      expect(bar.classes(cssClass)).toBe(true);
+      done();
+    }, 500);
   });
 });
